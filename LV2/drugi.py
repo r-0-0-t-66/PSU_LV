06c89167fe91a24795b36f9e39de79927f692e99
r-0-0-t-66 <128307593+r-0-0-t-66@users.noreply.github.com>
2023-04-04 19:19:49 +0200

    Update drugi.py

--- LV2/drugi.py
+++ LV2/drugi.py
@@ -1,14 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 data = np.loadtxt(open("/home/r00t/Radna površina/Python/LV2/mtcars.csv", "rb"), usecols=(1,2,3,4,5,6),delimiter=",",skiprows=1)
-mpg = data[:,1]
-hp = data[:, 4]
+mpg = data[:,0]
+hp = data[:, 3]
 wt = data[:, 5]
-cyl = data[:, 2]
+cyl = data[data[:, 1]==6]
 br_auta = 0
-plt.scatter(mpg, hp)
-plt.plot(wt,marker =".",markersize=10, color = "red")
+plt.scatter(hp, mpg, s = wt * 10)
+
 print(np.max(mpg))
 print(np.min(mpg))
 print(np.mean(mpg))
+
+mpg_6cyl_min = np.min(cyl[:, 0])
+mpg_6cyl_max = np.max(cyl[:, 0])
+mpg_6cyl_mean = np.mean(cyl[:, 0])
+
+print(np.max(mpg_6cyl_max))
+print(np.min(mpg_6cyl_min))
+print(np.mean(mpg_6cyl_mean))
+print("\n")
 plt.show()
